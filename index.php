2ef5e47853b764187fed476c9c86aec5baa5feb4
TeslaGateway B Grup <105518491+teslagate@users.noreply.github.com>
2022-05-14 06:39:59 +0300

    Update index.php
    update

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include'includes/header'; ?>
+<?php include'includes/header.php'; ?>
+
+<?php include'includes/company_section.php'; ?>
+
+<?php include'includes/trending_items.php'; ?>
+
+<?php include'includes/earn_income_section.php'; ?>
+
+<?php include'includes/footer.php'; ?>
