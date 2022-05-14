f6e92f636e46be4421d3fedd5c3b39de3d576427
TeslaGateway B Grup <105518491+teslagate@users.noreply.github.com>
2022-05-14 03:37:04 +0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MetaInv | Investment and earnings system with NFT</title>
+    <meta name="description" content="Investment and earnings system with NFT">
+        <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="@@dashboard">
+
+<div id="preloader"><i>.</i><i>.</i><i>.</i></div>
+
+<!-- main end  -->
+
+
+<div id="main-wrapper" class="front">
+
+    <div class="header landing">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="navigation">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="brand-logo">
+                            <a href="index.html">
+                                <img src="images/logo.png" alt="" class="logo-primary">
+                                <img src="images/logow.png" alt="" class="logo-white">
+                            </a>
+                        </div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item dropdown"><a class="nav-link" href="index.html">Home</a></li>
+                                
+                                <li class="nav-item"><a class="nav-link" href="#">Explore</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Companies</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">MetaInv?</a></li>
+                            
+                            </ul>
+                        </div>
+                        <div class="signin-btn d-flex align-items-center">
+                            <div class="dark-light-toggle theme-switch">
+                                <a class="btn btn-primary" href="#">Become an Investor</a>
+                            </div>
+
+                            <a class="btn btn-primary" href="#">Connect</a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- main end  -->
